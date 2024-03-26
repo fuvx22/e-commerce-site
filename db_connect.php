4a7b11091589ca6ec7c8ac00ac0333c765357105
fuvx22 <voxngocphu@gmail.com>
2024-03-26 16:42:37 +0700

    add gitignore

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database";
+$username = "root";
+$password = "";
+$dbname = "db_cuahangthoitrang";
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
